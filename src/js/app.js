798aa91d0d08dddfa63b472495ccce54b6d9a8b5
d3moid <d3moid@users.noreply.github.com>
2016-01-31 16:35:12 +0200

    refactor

--- src/js/app.js
+++ src/js/app.js
@@ -8,14 +8,17 @@ var model = {
 		.done(function( json ) {
 
 			model.data = json;
-			
+			// Contruct ViewModel after model is loaded
 			var vm = new ViewModel();
 			ko.applyBindings(vm);
 		})
 		.fail(function( jqxhr, textStatus, error ) {
 
 			var err = textStatus + ", " + error;
+			// Show error message
 			console.log( "Request Failed: " + err );
+			$('#quote').text(err);
+			$('#author').text('something went wrong');
 		});
 	}
 };
@@ -23,22 +26,23 @@ var model = {
 
 // -- VIEWMODEL -- //
 
-function ViewModel() {
+function ViewModel(err) {
 
 	var self = this;
-	
+
 	self.currentQuote = ko.observable();
 	
 	self.nextQuote = function() {
 		
+		// Get random author
 		if (!self.authorsMax) self.authorsMax = Object.keys(model.data.authors).length;
 		var aRandom = Math.floor(Math.random() * self.authorsMax);
 		var author = model.data.authors[aRandom].name;
-
+		// Get random quote from author
 		var quotesMax = Object.keys(model.data.quotes[aRandom]).length;
 		var qRandom = Math.floor(Math.random() * quotesMax);
 		var quote = model.data.quotes[aRandom][qRandom];
-
+		// Update current quote
 		self.currentQuote({ "author": author, "quote": quote });
 	};
 
