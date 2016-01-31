109d314e262a5dbbe4a0f35533660fb2d58b2658
d3moid <d3moid@users.noreply.github.com>
2016-01-31 11:28:17 +0200

    Map JSON to JS object.

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,27 @@
-
+// Load Quotes from JSON file
+var data;
 $.getJSON('js/quotes.json')
-	.done(function( data ) {
-		console.log(data);
+	.done(function( json ) {
+		data = json;
 	})
 	.fail(function( jqxhr, textStatus, error ) {
-			var err = textStatus + ", " + error;
+		var err = textStatus + ", " + error;
 		console.log( "Request Failed: " + err );
 });
+
+function ViewModel() {
+	var self = this;
+	self.test = true;
+}
+
+var model = {
+
+};
+
+var view = {
+
+};
+
+var vm = $.extend(new ViewModel(), data);
+ko.applyBindings(vm);
+console.log(vm.test);
