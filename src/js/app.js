94f3e8d8a1d66a204ac805b29a0e576dc6123c2c
d3moid <d3moid@users.noreply.github.com>
2016-01-31 12:37:40 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -26,13 +26,12 @@ function ViewModel() {
 
 	var self = this;
 	
-	self.availableAuthors = 1;
 	self.currentQuote = ko.observable();
 	
 	self.nextQuote = function() {
 		
 		var authorsMax = Object.keys(model.data.authors).length - 1;
-		var aRandom = Math.floor(Math.random() * authorMax);
+		var aRandom = Math.floor(Math.random() * authorsMax);
 		var author = model.data.authors[aRandom].name;
 
 		var quotesMax = Object.keys(model.data.quotes[aRandom]).length - 1;
@@ -41,7 +40,6 @@ function ViewModel() {
 
 		self.currentQuote({ "author": author, "quote": quote });
 	};
-	self.nextQuote();
 	
 	self.shareTwitter = function() {
 
