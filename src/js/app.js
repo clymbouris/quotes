78d49879178f078ec0f1aa8b0a003ee77e9dbe5b
d3moid <d3moid@users.noreply.github.com>
2016-01-31 11:04:09 +0200

    Remove wrapping function.

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,9 @@
-$(function(){
-	$.getJSON('js/quotes.json')
-		.done(function( data ) {
-			console.log(data);
-		})
-		.fail(function( jqxhr, textStatus, error ) {
-   			var err = textStatus + ", " + error;
-    		console.log( "Request Failed: " + err );
-		});
-})();
+
+$.getJSON('js/quotes.json')
+	.done(function( data ) {
+		console.log(data);
+	})
+	.fail(function( jqxhr, textStatus, error ) {
+			var err = textStatus + ", " + error;
+		console.log( "Request Failed: " + err );
+});
