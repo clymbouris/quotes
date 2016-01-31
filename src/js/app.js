67210cb1d61fbcc1fdd753346980d6fd9ed71845
d3moid <d3moid@users.noreply.github.com>
2016-01-31 10:59:40 +0200

    Add .done .fail methods on JSON request.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
 $(function(){
-	$.getJSON('js/quotes.json', function( data ) {
-	console.log(data);
-});
-}());
+	$.getJSON('js/quotes.json')
+		.done(function( data ) {
+			console.log(data);
+		})
+		.fail(function( jqxhr, textStatus, error ) {
+   			var err = textStatus + ", " + error;
+    		console.log( "Request Failed: " + err );
+		});
+})();
