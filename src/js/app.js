27c614fb495fe4ad8badedd73c53bf5df58db636
d3moid <d3moid@users.noreply.github.com>
2016-01-31 11:36:48 +0200

    Create MVVM structure.

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,46 @@
-// Load Quotes from JSON file
-var data;
-$.getJSON('js/quotes.json')
-	.done(function( json ) {
-		data = json;
-	})
-	.fail(function( jqxhr, textStatus, error ) {
-		var err = textStatus + ", " + error;
-		console.log( "Request Failed: " + err );
-});
 
-function ViewModel() {
-	var self = this;
-	self.test = true;
-}
+
+
+// -- MODEL -- //
 
 var model = {
-
+	data: null,
+	init: function() {
+		$.getJSON('js/quotes.json')
+		.done(function( json ) {
+			model.data = json;
+		})
+		.fail(function( jqxhr, textStatus, error ) {
+			var err = textStatus + ", " + error;
+			console.log( "Request Failed: " + err );
+		});
+	}
 };
+
+
+
+// -- VIEW -- //
 
 var view = {
 
 };
 
-var vm = $.extend(new ViewModel(), data);
+
+
+// -- VIEWMODEL -- //
+
+function ViewModel() {
+
+	var self = this;
+	
+	self.test = true;
+}
+
+
+
+// -- Initialize Components -- //
+
+model.init();
+var vm = new ViewModel();
 ko.applyBindings(vm);
 console.log(vm.test);
