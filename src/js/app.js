bcb9c2e2e13244fb23134447b2ce56abd4074b69
d3moid <d3moid@users.noreply.github.com>
2016-02-02 09:04:41 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -8,19 +8,21 @@ var model = {
 		.done(function( json ) {
 
 			model.data = json;
+
 			// Fade in/out custom binding
 			ko.bindingHandlers.fadeVisible = {
 			    init: function(element, valueAccessor) {
 			        // Initially set the element to be instantly visible/hidden depending on the value
 			        var value = valueAccessor();
-			        $('quote').fadeIn(); // Use "unwrapObservable" so we can handle values that may or may not be observable
+			        $('#quote').fadeIn(); // Use "unwrapObservable" so we can handle values that may or may not be observable
 			    },
 			    update: function(element, valueAccessor) {
 			        // Whenever the value subsequently changes, slowly fade the element in or out
 			        var value = valueAccessor();
-			        $('quote').fadeOut().fadeIn();
+			        $('#quote').fadeOut().fadeIn();
 			    }
 			};
+
 			// Contruct ViewModel after model is loaded
 			var vm = new ViewModel();
 			ko.applyBindings(vm);
@@ -43,7 +45,8 @@ function ViewModel(err) {
 
 	var self = this;
 	self.animations = true;
-	self.currentQuote = ko.observable();
+	self.currentQuote = ko.observable('');
+	self.currentAuthor = ko.observable('');
 	
 	self.nextQuote = function() {
 
@@ -56,12 +59,13 @@ function ViewModel(err) {
 		var qRandom = Math.floor(Math.random() * quotesMax);
 		var quote = model.data.quotes[aRandom][qRandom];
 		// Update current quote
-		self.currentQuote({ "author": author, "quote": quote });
+		self.currentQuote(quote);
+		self.currentAuthor(author);
 	};
 
 	self.tweetQuote = function() {
 		window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
-			encodeURIComponent('"' + self.currentQuote().quote + '" ' + self.currentQuote().author));
+			encodeURIComponent('"' + self.currentQuote() + ' - ' + self.author()));
     };
 }
 
