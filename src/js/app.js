6f0d0d73996579e0bbf6c142c28af1dccf8e6379
d3moid <d3moid@users.noreply.github.com>
2016-02-03 20:06:57 +0200

    f

--- src/js/app.js
+++ src/js/app.js
@@ -47,6 +47,10 @@ function ViewModel(err) {
 		if (!self.authorsMax) self.authorsMax = Object.keys(model.data.authors).length;
 		var aRandom = Math.floor(Math.random() * self.authorsMax);
 		var author = model.data.authors[aRandom].name;
+		// Make sure author is different every time
+		while (author === currentAuthor()) {
+			author = model.data.authors[aRandom].name;
+		}
 		// Get random quote from author
 		var quotesMax = Object.keys(model.data.quotes[aRandom]).length;
 		var qRandom = Math.floor(Math.random() * quotesMax);
